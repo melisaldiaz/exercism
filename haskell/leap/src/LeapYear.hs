caef1b1db8e2778207e50a3b3ae1a654f19b2eb3
melisaldiaz <melisaldiaz@gmail.com>
2020-01-22 16:40:20 +0200

    modified

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -3,4 +3,6 @@ module LeapYear
   ) where
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = or [mod year 4 == 0 && mod year 100 /= 0, mod year 400 == 0]
+isLeapYear year = 
+  or [ mod year 4 == 0 && mod year 100 /= 0
+     , mod year 400 == 0 ]
