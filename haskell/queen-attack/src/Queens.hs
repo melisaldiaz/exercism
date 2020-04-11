2b4c5e96e2102634acf180211934bf2e81a0f444
melisaldiaz <melisaldiaz@gmail.com>
2020-04-11 16:52:00 +0300

    haskell/queen-attack con tipos

--- haskell/queen-attack/src/Queens.hs
+++ haskell/queen-attack/src/Queens.hs
@@ -1,9 +1,24 @@
 module Queens (boardString, canAttack) where
 
+import Control.Monad (guard)
+
+data Board = Board {unBoard :: String}
+  deriving (Show)
+
 boardString :: Maybe (Int, Int) -> Maybe (Int, Int) -> String
 boardString yW yB =
-  let b0 = maybe emptyBoard (insertQueen 'W' (lines emptyBoard)) yW
-   in maybe b0 (insertQueen 'B' (lines b0)) yB
+  case boardString' yW yB of
+    Nothing -> error "invalid input"
+    Just x -> unBoard x
+
+boardString' :: Maybe (Int, Int) -> Maybe (Int, Int) -> Maybe Board
+boardString' yW yB = do
+  b0 <- case yW of
+    Nothing -> Just emptyBoard
+    Just w -> insertQueen 'W' emptyBoard w
+  case yB of
+    Nothing -> Just b0
+    Just b -> insertQueen 'B' b0 b
 
 canAttack :: (Int, Int) -> (Int, Int) -> Bool
 canAttack (ax, ay) (bx, by)
@@ -11,17 +26,38 @@ canAttack (ax, ay) (bx, by)
   | ay == by = True
   | otherwise = abs (ax - bx) == abs (ay - by)
 
-insertQueen :: Char -> [String] -> (Int, Int) -> String
-insertQueen queen board (x, y)
-  | x > 7 || y > 7 = ""
-  | otherwise =
-    unlines $
-      take x board
-        ++ [replaceAt (y * 2) queen rank]
-        ++ drop (x + 1) board
+insertQueen :: Char -> Board -> (Int, Int) -> Maybe Board
+insertQueen queen board (x, y) =
+  boardFromLines $
+    take x (boardToLines board)
+      ++ [replaceAt (y * 2) queen rank]
+      ++ drop (x + 1) (boardToLines board)
 
-emptyBoard :: String
-emptyBoard = unlines $ replicate 8 rank
+emptyBoard :: Board
+emptyBoard = Board $ unlines $ replicate 8 rank
+
+boardToLines :: Board -> [String]
+boardToLines = lines . unBoard
+
+boardFromLines :: [String] -> Maybe Board
+boardFromLines x = do 
+  guard (length x == 8)
+  guard (all validRank x)
+  pure (Board (unlines x))
+
+validRank :: String -> Bool
+validRank s
+  | s == rank = True
+  | otherwise = f s
+  where
+    f :: String -> Bool
+    f x = case x of
+      "" -> False
+      a : [] -> a /= ' '
+      a : b : rest ->
+        if a /= ' ' && b == ' '
+          then f rest
+          else False
 
 rank :: String
 rank = "_ _ _ _ _ _ _ _"
