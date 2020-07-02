20d64ae5f30de59c9c9f094ae6597ebd610ad4f5
melisaldiaz <melisaldiaz@gmail.com>
2020-07-02 17:36:30 +0300

    add rna-transcription

---
+++ typescript/rna-transcription/rna-transcription.ts
@@ -0,0 +1,60 @@
+class Transcriptor {
+    toRna(strand: string): string {
+        const input: string[] = strand.split('');
+        let result: string[] = [];
+
+        if (any(nonNucleotide, input)) { throw new Error('Invalid input DNA.') }
+
+        else {
+            for (let i = 0; i < input.length; i++) {
+                result.push(toRNAnuc(input[i]))
+            }
+        } return result.join('');
+    }
+}
+
+// Returns the RNA complement to the given DNA nucleotide.
+function toRNAnuc(x: string): string {
+    let result: string = '';
+
+    switch (x) {
+        case 'G': result = 'C';
+            break;
+
+        case 'C': result = 'G';
+            break;
+
+        case 'T': result = 'A';
+            break;
+
+        case 'A': result = 'U';
+            break;
+
+    } return result;
+}
+
+
+// Determines if a character represents a Nucleotide.
+function isNucleotide(n: string): boolean {
+    const nucleotides: string[] = ['A', 'C', 'G', 'T']
+
+    for (let i = 0; i < nucleotides.length; i++) {
+        if (n === nucleotides[i]) { return true; }
+    }
+    return false;
+}
+
+function nonNucleotide(n: string): boolean {
+    return !isNucleotide(n)
+}
+
+// Determines whether any element of the structure satisfies the predicate.
+function any(predicate: (x: any) => boolean, x: any[]): boolean {
+    for (let i = 0; i < x.length; i++) {
+        if (predicate(x[i])) { return true }
+    }
+    return false;
+}
+
+
+export default Transcriptor
